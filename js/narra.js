e2b0b3cd8e3571605911f202fcce7f95079ada8f
Srikahn <49712202+Srikahn@users.noreply.github.com>
2020-09-15 18:53:26 +0200

    Basic displaying of sequences

---
+++ js/narra.js
@@ -0,0 +1,58 @@
+/* Namespace */
+var Narra = {}
+
+Narra.Story = class {
+    constructor(name) {
+        var thisStory = this;
+
+        this.parent = $("body");
+        this.content = null;
+
+        var path = "stories/" + name + ".json";
+        console.log("Trying to load " + path);
+
+       $.getJSON( path, function(data) {
+            console.log("Story loaded!");
+            console.log(data);
+            thisStory.start(data);
+        })
+        .fail(function(data) {
+            console.log("Failing to load the story");
+            console.log(data);
+        })
+    }
+
+    start(data) {
+        this.content = data;
+
+        this.display = createDOM("div", "narra-main");
+        this.parent.append(this.display);
+
+        //TODO: setup the basic configuration
+        this.loadSequence(this.content.configuration.start);
+    }
+
+    loadSequence(name) {
+        var thisStory = this;
+        console.log("Displaying sequence " + name);
+
+        var sequence = this.content[name];
+        console.log(sequence);
+
+        var sequenceDOM = createDOM("div", "sequence");
+        var textDOM = createDOM("div", "text", sequence.text);
+        var choicesDOM = createDOM("ul", "choices");
+        this.display.append(sequenceDOM);
+        sequenceDOM.append(textDOM, choicesDOM);
+
+        if(sequence.choices != null) {
+            $.each(sequence.choices, function( key, choice ) {
+                var choiceDOM = createDOM("li", "choice", choice.text);
+                choicesDOM.append(choiceDOM);
+                choiceDOM.click(function() {
+                    thisStory.loadSequence(choice.destination);
+                });
+            });
+        }
+    }
+}
